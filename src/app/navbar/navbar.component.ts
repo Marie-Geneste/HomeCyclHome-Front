9384cfb6ab1805d270ee91693f38b7d018622fc1
Marie-Geneste <marie.e.geneste@gmail.com>
2025-01-06 16:25:50 +0100

    navbar done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule,  TieredMenuModule, TooltipModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  items: MenuItem[] | undefined;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+      this.items = [
+          {
+            label: 'Accueil',
+            icon: 'pi pi-home',
+            route: '/'
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Produits',
+            icon: 'pi pi-box',
+            route: '/products'
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Profil',
+            icon: 'pi pi-user',
+            route: '/profile'
+          }
+      ]
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
