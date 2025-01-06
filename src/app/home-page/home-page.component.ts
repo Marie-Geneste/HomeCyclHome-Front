ed0488d65cfa3c3463458118bd9ba91d36c899c0
Marie-Geneste <marie.e.geneste@gmail.com>
2025-01-06 22:49:52 +0100

    WIP homepage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ CardModule, ButtonModule, DividerModule, InputTextModule ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
